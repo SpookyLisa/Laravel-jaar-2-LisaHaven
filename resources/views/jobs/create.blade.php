6f81da2d9b210f5cab6a04d0946fa2c575fa4448
Lisa Haven <lisahaven2008@gmail.com>
2025-10-30 18:02:45 +0100

    video-20- login en registration

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,53 +1,38 @@
 <x-layout>
     <x-slot:heading>
         Create Jobs
-     </x-slot:heading>
+    </x-slot:heading>
 
     <form method="POST" action="/jobs">
         @csrf
+
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base/7 font-semibold text-gray-900">Create a new job</h2>
                 <p class="mt-1 text-sm/6 text-gray-600">Insert the info</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
+
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="title" type="text" name="title" placeholder="Job" required
-                                    class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
-                            </div>
+                               <x-form-input name="title" id="title" placeholder="Job title" required></x-form-input>
 
-                            @error('title')
-                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-                            @enderror
+                            <x-form-error name="title"></x-form-error>
                         </div>
-                    </div> 
-                <div class="sm:col-span-4">
-                                    <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
-                                    <div class="mt-2">
-                                        <div
-                                            class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                            <input id="salary" type="text" name="salary" placeholder="Job salary" required
-                                                class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
-                                        </div>
-                                        @error('salary')
-                                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-                                        @enderror
-                                    </div>
-                                </div> 
-                            </div> 
+                         </x-form-field>
 
-                        </div> 
-                    </div>
-                <div class="mt-6 flex items-center justify-end gap-x-6">
-                        <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-                        <button type="submit"
-                            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-                    </div>
-                </form>
-            </x-layout>
-
-
+                    <x-form-field>
+                        <x-form-label for="salary">Salary</x-form-label>
+                <div class="mt-2">
+                    </x-form-field>
+                      </div>
+                </div>
+            </div>
+        </div>
+    <div class="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
+            <x-form-button>Save</x-form-button>
+        </div>
+    </form>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
+
 Route::resource('jobs', JobController::class);
+
+Route::get('/register', [RegisteredUserController::class,'create']);
+Route::post('/register', [RegisteredUserController::class,'store']);
+
+Route::get('/login', [SessionController::class,'create']);
+Route::post('/login', [SessionController::class,'store']);
