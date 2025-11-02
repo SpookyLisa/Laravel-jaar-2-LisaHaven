eefee88b895fdf91c57d73db31bc6741ffeb07ba
Lisa Haven <lisahaven2008@gmail.com>
2025-11-02 15:49:37 +0100

    Video-29-Jobs, Tags, TDD

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,19 +1,21 @@
-<x-panel class="flex gap-x-6">
-    <div>
-        <x-employer-logo />
-    </div>
+@props(['job'])
 
-    <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400 transition-colors duration-300">Laracasts</a>
+    <x-panel class="flex gap-x-6">
+        <div>
+            <x-employer-logo />
+        </div>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">Video Producer</h3>
+        <div class="flex-1 flex flex-col">
+            <a href="#" class="self-start text-sm text-gray-400 transition-colors duration-300">{{ $job->employer->name }}</a>
 
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
-    </div>
+            <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">{{ $job->title }}</h3>
 
-    <div>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-    </div>
-</x-panel>
+            <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
+        </div>
+
+        <div>
+            @foreach($job->tags as $tag)
+            <x-tag :$tag />
+            @endforeach
+        </div>
+    </x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,19 +1,21 @@
-<x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
+@props(['job'])
+ 
+ <x-panel class="flex flex-col text-center">
+        <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
-    <div class="py-8">
-        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
-        <p class="text-sm mt-4">Full Time - From $60,000</p>
-    </div>
-
-    <div class="flex justify-between items-center mt-auto">
-        <div>
-            <x-tag size="small">Backend</x-tag>
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">Manager</x-tag>
+        <div class="py-8">
+            <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
+            <p class="text-sm mt-4">{{ $job->salary }}</p>
         </div>
 
-         <x-employer-logo :width="42" />
-    </div>
-</x-panel>
+        <div class="flex justify-between items-center mt-auto">
+            <div>
+                @foreach($job->tags as $tag)
+                <x-tag :$tag size="small" />
+                @endforeach
+            </div>
+
+            <x-employer-logo :width="42" />
+        </div>
+    </x-panel>
 
